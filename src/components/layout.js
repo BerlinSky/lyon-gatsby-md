fcf6f954e50bd7d5c348f2f2a8d5bc4015c3c884
Berlin Sky <pedroyoung8989@gmail.com>
2019-08-11 11:29:37 -0700

    Initial setup

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,27 +1,27 @@
 /**
  * Layout component that queries for data
  * with Gatsby's useStaticQuery component
- *
+ *                   
  * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
+ */ 
+    
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
+                      
 import Header from "./header"
 import "./layout.css"
-
+               
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
-      site {
+      site {   
         siteMetadata {
           title
-        }
-      }
-    }
-  `)
+        } 
+      }   
+    } 
+  `)  
 
   return (
     <>
@@ -39,14 +39,15 @@ const Layout = ({ children }) => {
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
-}
-
+        </footer>             
+      </div>           
+    </>                   
+  )                  
+}             
+                       
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
-
+}                
+                 
 export default Layout
+        
