77a79b2fab3a6beb18d9262878b0f15197f307ab
Berlin Sky <pedroyoung8989@gmail.com>
2019-08-11 12:50:20 -0700

    update styles

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,14 +1,15 @@
 // import { link } from "gatsby"
 // import PropTyples from "prop-types"
 import React from "react"             
-
-const Footer = () => (
-  <footer>
+import footerStyles from './footer.module.scss'
+                                               
+const Footer = () => (                     
+  <footer className={footerStyles.footer}>        
     This is the footer: © {new Date().getFullYear()}, Built with
-    {` `}
+    {` `}                                      
     <a href="https://www.gatsbyjs.org">Gatsby</a>
-  </footer>
-)
- 
+  </footer>     
+)               
+               
 export default Footer
- 
+               
