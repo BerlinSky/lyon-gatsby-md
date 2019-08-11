563ac17b9ae6f29bcd22164361e8f0d12edaacb9
Berlin Sky <pedroyoung8989@gmail.com>
2019-08-11 11:47:28 -0700

    Set up the sass preprocessor

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,14 +3,14 @@
  * with Gatsby's useStaticQuery component
  *                   
  * See: https://www.gatsbyjs.org/docs/use-static-query/
- */ 
-    
+ */                  
+                  
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
                       
 import Header from "./header"
-import "./layout.css"
+import "./layout.scss"
                
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
