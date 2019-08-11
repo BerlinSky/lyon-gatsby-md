dd5c40fc5d6c2cfccab5375adf4924f2c9abd825
Berlin Sky <pedroyoung8989@gmail.com>
2019-08-11 12:41:39 -0700

    Update layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,51 +1,53 @@
 /**
  * Layout component that queries for data
  * with Gatsby's useStaticQuery component
- *                           
+ *
  * See: https://www.gatsbyjs.org/docs/use-static-query/
- */                          
-                             
-import React from "react"    
+ */
+
+import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-                             
+
 import Header from "./header/header"
 import Footer from "./footer/footer"
-                             
-import "./layout.scss"       
-                       
+
+import "./layout.scss"
+import layoutStyles from "./layout.module.scss"
+
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
-      site {          
+      site {                                  
         siteMetadata {
           title
-        }    
-      }   
-    } 
-  `)                    
-                        
-  return (              
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div            
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}         
-      >           
-        <main>{children}</main>
-      </div>           
-      <Footer />            
-    </>                   
-  )                  
-}               
-                       
+        }
+      }
+    }
+  `)
+
+  return (
+    <section className={layoutStyles.layout}>
+      <div className={layoutStyles.container}>
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <div
+          style={{
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `0px 1.0875rem 1.45rem`,
+            paddingTop: 0,
+          }}
+        >
+          <main>{children}</main>
+        </div>
+        <Footer />
+      </div>
+    </section>
+  )
+}
+
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}                
-                 
+}
+
 export default Layout
-          
