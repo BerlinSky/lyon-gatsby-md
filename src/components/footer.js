aa464a707e0107a6a22358ca002ebe42054fe779
Berlin Sky <pedroyoung8989@gmail.com>
2019-08-11 12:09:00 -0700

    update footer

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import { link } from "gatsby"
+import PropTyples from "prop-types"
+import React from "react"
+
+const Footer = () => (
+  <footer>
+    This is the footer: © {new Date().getFullYear()}, Built with
+    {` `}
+    <a href="https://www.gatsbyjs.org">Gatsby</a>
+  </footer>
+)
+
+export default Footer
