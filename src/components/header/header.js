e0e4fb0465a07d5344fb793f94ef888f932ee082
Berlin Sky <pedroyoung8989@gmail.com>
2019-08-11 13:39:31 -0700

    update the layout

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,23 +1,28 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"       
-                                 
-import headerStyle from './header.module.scss'
-                                
-const Header = ({ siteTitle }) => (           
-  <header className={headerStyle.header}>                          
-    <h1 style={{ margin: 0 }}>    
-      <Link to="/" className={headerStyle.header}>                           
-        {siteTitle}                             
-      </Link>                   
-    </h1>                       
-  </header>                     
-)                             
-                              
-Header.propTypes = {          
+import React from "react"
+
+import headerStyle from "./header.module.scss"
+
+const Header = ({ siteTitle }) => (
+  <header className={headerStyle.header}>
+    <h1 style={{ margin: 0 }}>
+      <Link to="/" className={headerStyle.header}>
+        {siteTitle}
+      </Link>
+      <ul>
+        <li>
+          <Link to="./post">Posts</Link>
+        </li>
+      </ul>
+    </h1>
+  </header>
+)        
+
+Header.propTypes = {
   siteTitle: PropTypes.string,
-}                             
-                    
+}
+
 Header.defaultProps = {
   siteTitle: ``,
 }
