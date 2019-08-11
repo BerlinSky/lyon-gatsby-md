22d7f31e8beed502bde16411f9ae6db403f5bb97
Berlin Sky <pedroyoung8989@gmail.com>
2019-08-11 12:18:26 -0700

    update layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,15 +9,15 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
                              
-import Header from "./header"
+import Header from "./header/header"
 import Footer from "./footer/footer"
+                             
+import "./layout.scss"       
                        
-import "./layout.scss" 
-                      
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
-      site {   
+      site {          
         siteMetadata {
           title
         }    
