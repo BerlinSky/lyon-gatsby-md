e351ec73b13c75e8e50a2fe655d72055f9b5284c
Berlin Sky <pedroyoung8989@gmail.com>
2019-08-12 16:35:05 -0700

    Update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,49 +2,61 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Mini Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,          
-  },                              
-  plugins: [                   
-    `gatsby-plugin-sass`,                 
+    author: `@gatsbyjs`,
+  },
+  plugins: [
+    `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
-    {                            
+    {
       resolve: `gatsby-source-filesystem`,
-      options: {                
-        name: `images`,        
+      options: {
+        name: `images`,
         path: `${__dirname}/src/images`,
-      },                       
-    },                         
+      },
+    },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,     
-    {      
+    `gatsby-plugin-sharp`,
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
-        theme_color: `#663399`,                     
-        display: `minimal-ui`,                     
+        theme_color: `#663399`,
+        display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },                                          
-    },                                           
-    `gatsby-transformer-remark`,                
-    {                                          
-      resolve: `gatsby-source-filesystem`,    
-      options: {                             
-        name: `markdown-pages`,             
-        path: `${__dirname}/src/`
-      }                                    
-    },                
-    {   
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
       },
-    },                   
+    },
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/`,
+      },
+    },                         
+    {                                
+      resolve: `gatsby-plugin-typography`,
+      options: {                    
+        pathToConfigModule: `src/utils/typography`,
+      },                                
+    },                               
+    {                               
+      resolve: `gatsby-plugin-manifest`,
+      options: {                   
+        name: "GatsbyJS Starter", 
+        short_name: "GatsbyJS Starter",
+        start_url: "/",          
+        display: "standalone",  
+        icon: "src/images/favicon.png", 
+        crossOrigin: `use-credentials`,
+      },                        
+    },                        
+                             
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,          
-  ],                                    
-}                                      
-                                      
+    // `gatsby-plugin-offline`,
+  ],                        
+}                          
+                        
