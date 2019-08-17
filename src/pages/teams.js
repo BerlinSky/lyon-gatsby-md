2acafa33424774a4f006b5ffa337067463ae9f83
Berlin Sky <pedroyoung8989@gmail.com>
2019-08-17 11:10:54 -0700

    Updates

--- src/pages/teams.js
+++ src/pages/teams.js
@@ -1,22 +1,22 @@
 import React from "react"
 // import { Link, graphql } from "gatsby"
-import { graphql } from "gatsby" 
-                                  
-import Layout from "../components/layout"
-import TeamPage from "./teams/teams"        
-                                         
-const TeamsLandingPage = ({data}) => (           
-  <Layout>                           
-    <TeamPage data={data}></TeamPage>  
-  </Layout>                          
-)                               
-                                
-export const query = graphql`   
-  query {                       
+import { graphql } from "gatsby"                                            
+                                                           
+import Layout from "../components/layout"        
+import TeamPage from "./teams/teams-list"        
+                                     
+const TeamsLandingPage = ({data}) => (              
+  <Layout>                                          
+    <TeamPage totalCount={data.allMarkdownRemark.totalCount} edges={data.allMarkdownRemark.edges}></TeamPage>   
+  </Layout>                                                          
+)                                                                 
+                                                                  
+export const query = graphql`                                     
+  query {                              
     allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      totalCount               
-      edges {                
-        node {              
+      totalCount                    
+      edges {                   
+        node {                 
           id                
           frontmatter {     
             title           
---
+++ src/pages/teams/teams-list.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { Link } from "gatsby"                        
+                                                                         
+import styles from "./teams.module.scss";                            
+                                                                     
+const TeamsPage = (props) => (              
+                                       
+  <div className={ styles.teams }>                                 
+    <h1 className={ styles.title }>Welcome to the Teams Page</h1>                       
+    <p>Our {props.totalCount} teams</p>     
+                                                  
+    <ul>                                                              
+      {props.edges.map(({ node }) => (                    
+        <li key={node.id}>                                            
+          <Link to={node.fields.slug}>                               
+            <h3>                                                    
+              {node.frontmatter.title}{" "}                        
+              <span>                                             
+                — {node.frontmatter.date}                   
+              </span>                                 
+            </h3>                                
+            <p>{node.excerpt}</p>                
+          </Link>                                
+        </li>                                   
+      ))}        
+    </ul>                
+  </div>      
+)             
+              
+export default TeamsPage
+              
