0a3345bb70f66250d77db7f42ba637e3001c61e3
Berlin Sky <pedroyoung8989@gmail.com>
2019-08-17 14:32:54 -0700

    updates

--- src/pages/markdown-pages/germany/german-beer.md
+++ src/pages/markdown-pages/germany/german-beer.md
@@ -6,3 +6,15 @@ excerpt: "A preview of my third post"
 ---
 
 ## Great Gatsby is memorable.
+
+I just launched a new bootcamp!
+
+<!-- ![Grass](./grass.png) -->
+
+![News](./stock_01.jpg)
+
+## Topics Covered
+
+1. Gatsby
+2. GraphQL
+3. React
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,25 +1,25 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
-                                      
-import styles from "./posts.module.scss"
-                          
+                                         
+import styles from "./posts.module.scss" 
+                                         
 export default ({ data }) => {          
   const post = data.markdownRemark    
-  return (                           
-    <Layout>                  
+  return (                            
+    <Layout>                          
       <div className={ styles.main }>                  
         <h1>{post.frontmatter.title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
-      </div>                       
-    </Layout>                 
-  )                          
-}                  
-                   
-export const query = graphql`
+        <div className={ styles.mdContent }  dangerouslySetInnerHTML={{ __html: post.html }} />
+      </div>                         
+    </Layout>                       
+  )                                
+}                                 
+                                 
+export const query = graphql`  
   query($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
-      html
+      html         
       frontmatter {
         title
       }
