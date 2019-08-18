dc8769070b6e7a23d6f99a77e6248100b12f0494
Berlin Sky <pedroyoung8989@gmail.com>
2019-08-17 22:04:07 -0700

    Add gatsby-background-image feature

---
+++ src/pages/contacts.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { graphql, StaticQuery } from 'gatsby'
+import styled from 'styled-components'           
+                                     
+import BackgroundImage from 'gatsby-background-image'     
+                                                        
+const BackgroundSection = ({ className }) => (          
+    <StaticQuery query={graphql`                        
+      query {                                           
+        desktop: file(relativePath: { eq: "stock_13.jpg" }) {
+          childImageSharp {                                   
+            fluid(quality: 90, maxWidth: 4160) {             
+              ...GatsbyImageSharpFluid_withWebp             
+            }                                              
+          }                                               
+        }                                                
+      }                                                 
+    `}                                                 
+     render={data => {                                
+       const imageData = data.desktop.childImageSharp.fluid
+       return (                                      
+          <BackgroundImage                          
+            Tag="section"                          
+            className={className}
+            fluid={imageData}    
+            backgroundColor={`#040e18`}
+          >                                  
+            <h1>Hello gatsby-background-image</h1>
+          </BackgroundImage>              
+       )                             
+     }}                          
+    />                                
+)                                     
+                                      
+const StyledBackgroundSection = styled(BackgroundSection)`
+  width: 100%;                        
+  height: 400px;                
+  background-position: bottom center;
+  background-repeat: repeat-y;
+  background-size: cover;
+`                        
+                   
+export default StyledBackgroundSection
