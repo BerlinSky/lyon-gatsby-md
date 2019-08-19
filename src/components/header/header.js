736365ef1f11c49554fd890e0cc867fbd16322e5
Berlin Sky <pedroyoung8989@gmail.com>
2019-08-19 07:57:38 -0700

    Update logo

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,25 +1,25 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"    
-import React from "react"               
-                                           
+import PropTypes from "prop-types"          
+import React from "react"                    
+                                            
 import logo from '../../images/site-logo.png'
-                                           
+                                            
 import headerStyle from "./header.module.scss"
                                               
-const Header = ({ siteTitle }) => (     
+const Header = ({ siteTitle }) => (        
   <header className={ headerStyle.header }>
-    <nav id={siteTitle}>                   
-      <div >                           
-        <img src={logo} alt="Logo" />
-      </div>                      
-      <ul>                               
-        <li>                             
-          <Link to="/">Home</Link>      
-        </li>                     
+    <nav id={siteTitle}>                    
+      <div className={ headerStyle.imageBox }>                           
+        <img src={logo} alt="Logo" />       
+      </div>                                
+      <ul>                                 
+        <li>                               
+          <Link to="/">Home</Link>        
+        </li>                        
         <li>                                  
           <Link to="/services/">Services</Link>
-        </li>                     
-        <li>                   
+        </li>                        
+        <li>                      
           <Link to="/teams/">Teams</Link>
         </li>                  
         <li>                   
